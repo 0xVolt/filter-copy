b51b17b7b617bb61b72654057f233fa8fb27b795
Desh Iyer <desh.iyer@gmail.com>
2023-02-11 14:44:29 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -13,53 +13,52 @@ int main(int argv, char *argc[])
 
     // FIR filter with circular buffer
     // Declaring the filter struct variable
-    // FIRFilter fir;
+    FIRFilter fir;
 
-    // // Initialise the filter coefficient (the weight)
-    // FIRFilter_init(&fir);
+    // Initialise the filter coefficient (the weight)
+    FIRFilter_init(&fir);
 
-    // // Calculating the filtered values
-    // for (int i = 0; i < n; i++)
-    // {
-    //     FIRFilter_calc(&fir, input[i]);
-    //     filteredInput[i] = fir.out;
-    // }
+    // Calculating the filtered values
+    for (int i = 0; i < n; i++) {
+        FIRFilter_calc(&fir, input[i]);
+        filteredInput[i] = fir.out;
+    }
 
-    // // Plotting with GNU Plot
-    // FILE *gnuplot = fopen("commands.p", "w");
+    // Plotting with GNU Plot
+    FILE *gnuplot = fopen("commands.p", "w");
 
-    // char title[200] = "";
-    // char xLabel[200] = "";
-    // char yLabel[200] = "";
+    char title[200] = "";
+    char xLabel[200] = "";
+    char yLabel[200] = "";
 
-    // // Set the plot title, xlabel and ylabel
-    // sprintf(title, "Original and filtered samples");
-    // sprintf(xLabel, "Time");
-    // sprintf(yLabel, "Amplitude");
+    // Set the plot title, xlabel and ylabel
+    sprintf(title, "Original and filtered samples");
+    sprintf(xLabel, "Time");
+    sprintf(yLabel, "Amplitude");
 
-    // fprintf(gnuplot, "set terminal wxt size 500, 400\n");
-    // fprintf(gnuplot, "set title '%s'\n", title);
-    // fprintf(gnuplot, "set xlabel '%s'\n", xLabel);
-    // fprintf(gnuplot, "set ylabel '%s'\n", yLabel);
+    fprintf(gnuplot, "set terminal wxt size 500, 400\n");
+    fprintf(gnuplot, "set title '%s'\n", title);
+    fprintf(gnuplot, "set xlabel '%s'\n", xLabel);
+    fprintf(gnuplot, "set ylabel '%s'\n", yLabel);
 
-    // // Plot the data
-    // fprintf(gnuplot, "plot '-' w lines lc rgb 'blue' title \"sampled data\", '-' w lines lc rgb 'red' title \"Filtered data\"\n");
-    // for (int i = 0; i < n; i++)
-    // {
-    //     fprintf(gnuplot, "%d %lf\n", i, input[i]);
-    // }
-    // fprintf(gnuplot, "e\n");
-    // for (int i = 0; i < n; i++)
-    // {
-    //     fprintf(gnuplot, "%d %lf\n", i, filteredInput[i]);
-    // }
-    // fprintf(gnuplot, "e\n");
-    // fprintf(gnuplot, "replot\n");
+    // Plot the data
+    fprintf(gnuplot, "plot '-' w lines lc rgb 'blue' title \"sampled data\", '-' w lines lc rgb 'red' title \"Filtered data\"\n");
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(gnuplot, "%d %lf\n", i, input[i]);
+    }
+    fprintf(gnuplot, "e\n");
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(gnuplot, "%d %lf\n", i, filteredInput[i]);
+    }
+    fprintf(gnuplot, "e\n");
+    fprintf(gnuplot, "replot\n");
 
-    // // Close the GNU plot piping
-    // fclose(gnuplot);
+    // Close the GNU plot piping
+    fclose(gnuplot);
 
-    // cout << "Generated commands.p file!\n";
+    cout << "Generated commands.p file!\n";
     
     return 0;
 }
